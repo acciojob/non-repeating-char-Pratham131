ac11b849bb79c109e3c480c0bbc1a1bf1088fa75
Pratham131 <104488785+Pratham131@users.noreply.github.com>
2023-07-16 18:54:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 //your JS code here. If required.
 let ans = prompt("Enter a string")
+
+let obj = {}
+
+for(let i = 0; ans.length; i++){
+	if(obj[str[i]]){
+		obj[str[i]]++
+	}
+	else {
+		obj[str[i]] = 1;
+	}
+}
+
+for (let index = 0; index < ans.length; index++) {
+	if(obj[str[index]] === 1){
+		alert(str[index]);
+		break;
+	}
+}
+
+
 alert(${ans})
