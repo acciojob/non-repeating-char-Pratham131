d9482f5929f15ea1e953a80e76e96684a3e27a59
Pratham131 <104488785+Pratham131@users.noreply.github.com>
2023-07-18 20:03:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,13 +11,26 @@ for(let i = 0; i < ans.length; i++){
 		obj[ans[i]] = 1;
 	}
 }
+let firstNonRepeatedChar = null;
 
 for (let index = 0; index < ans.length; index++) {
-	if(obj[ans[index]] === 1){
-		alert(`${ans[index]}`);
-		break;
-	}
+  if (obj[ans[index]] === 1) {
+    firstNonRepeatedChar = ans[index];
+    break;
+  }
 }
 
+if (firstNonRepeatedChar) {
+  alert(firstNonRepeatedChar);
+} else {
+  alert("No non-repeated characters found.");
+}
+// for (let index = 0; index < ans.length; index++) {
+// 	if(obj[ans[index]] === 1){
+// 		alert(`${ans[index]}`);
+// 		break;
+// 	}
+// }
 
-alert(${ans})
+
+// alert(${ans})
